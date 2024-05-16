0b78cfe32c925173ea99425957b68481e91e7d38
harsh pandey <harsh.pandey77@gmail.com>
2024-05-16 17:43:55 +0530

    Initial Commit

---
+++ AnimationDataManager/AnimationDataManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDataManagerApp.swift
+//  AnimationDataManager
+//
+//  Created by harsh  on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDataManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
